ab184eae377ae7e226b632ee0ddebd4f0b871b38
Liliia <110099141+LiliiaVol@users.noreply.github.com>
2023-05-29 20:05:40 +0300

    Update scriptTEST.js

--- test/scriptTEST.js
+++ test/scriptTEST.js
@@ -2,103 +2,67 @@ const ul = document.querySelector('ul');
 const items = [];
 const inputTextElement = document.querySelector('.textInput');
 const wrapper = document.querySelector('.wrapper');
-const div = document.querySelector('.red-color');
 
-function addItemToArr(item) {
-  items.push(item.textContent);
-  console.log(items);
+function addItem() {
+  items.push(inputTextElement.value);
   inputTextElement.value = "";
+  const myItemsStrAfter = JSON.stringify(items);
+  localStorage.setItem('items', myItemsStrAfter)
+
+  console.log(items)
 };
 
 function addNewElemenet() {
   const li = document.createElement('li');
   const deleteButton = document.createElement('button');
-// !!!!!!!!!!!!!!!!!!!!!!!!!
-  let lis = document.querySelector(".wrapper").getElementsByTagName("li");
-  // console.log(lis)
-// !!!!!!!!!!!!!!!!!!!!!!!!!
+  const viewButton = document.createElement('button');
+
   deleteButton.addEventListener("click", (event) => {
-
-    // console.log(event.target.parentElement.textContent)
-    // event.target.parentElement.value = "1"
-    
-    let i = 0;
-    while (i < items.length) {
-      if (items[i] === event.target.parentElement.textContent) {
-        items.splice(i, 1);
-        break;
-      } else {
-        ++i;
-      }
-    }
-
-    // for (let i = 0; i < items.length; i++) {
-    //   if (event.target.parentElement.textContent === items[i]) {
-    //     items.indexOf(items[i])
-    //   }
-    // }
-    console.log("target", event.target);
     event.target.parentElement.remove();
-// !!!!!!!!!!!!!!!!!!!!!!!!!
-    let index = event.target.parentElement;
-    console.log(index);
-// !!!!!!!!!!!!!!!!!!!!!!!!!
-    // items = items.filter(el => el.id !== event.target.id);
-    // event.target.style.backgroundColor = 'blue';
   });
+
+  viewButton.addEventListener("click", (event) => {
+    const item = event.target.parentElement;
+    localStorage.setItem('item', item);
+    window.location='view.html';
+  })
+  
 
   let inputTextElement = document.querySelector('.textInput');
   li.innerHTML = inputTextElement.value;
   deleteButton.innerText = 'delete';
-  deleteButton.classList.add('list-items-button');
-
-  // deleteButton.addEventListener('onclick', deleteItem())
-  // deleteButton.setAttribute("onclick", test);
-  // deleteButton.onclick = test;
+  viewButton.innerHTML = 'view';
+  viewButton.classList.add('list-items-button');
+  deleteButton.classList.add('list-items-button-delete');
 
   li.append(deleteButton);
+  li.append(viewButton);
   li.classList.add('list-items');
   ul.append(li);
-  addItemToArr(li);
+  addItem();
 };
 
 inputTextElement.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    console.log('Enter key pressed');
     addNewElemenet();
   };
 });
 
-const deleteItemFromArr = () => {
-  console.log("deleteItem");
-};
+// function getInformationAboutItem() {
 
-// div.addEventListener("click", setBackground);
-
-// function setBackground(color) {
-//   div.style.backgroundColor = 'color';
-// };
-// function setBackground() {
-//   this.style.backgroundColor = 'red';
-// };
-
-// div.addEventListener('mouseover', setBackground('green'))
-// // div.addEventListener('mouseout', setBackground)
-
-
-// div.addEventListener('click', changeColorBlue)
-
-// function changeColorBlue() {
-//   div.style.backgroundColor = 'blue';
-// };
-
-// div.addEventListener('dblclick', changeColorPurple)
-
-// function changeColorPurple() {
-//   div.style.backgroundColor = 'purple';
 // };
 
 // HERE IS CEMETERY FOR CODE WHICH I DON'T USE BUT MAYBE I WILL NEED HIM IN FUTERE
 // const test = function(e) {
 //   e.target.parentElement.remove();
 // };
+
+// let i = 0;
+//     while (i < items.length) {
+//       if (items[i] === event.target.parentElement.textContent) {
+//         items.splice(i, 1);
+//         break;
+//       } else {
+//         ++i;
+//       }
+//     }
